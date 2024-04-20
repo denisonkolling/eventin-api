be4fe5292f5ff5a870b53d502f6e2fff39485cb9
Denison Kolling <denisonkolling@gmail.com>
2024-04-19 21:13:51 -0300

    feat: add registred attendees count

--- src/routes/get-event.ts
+++ src/routes/get-event.ts
@@ -4,46 +4,58 @@ import { prisma } from '../lib/prisma';
 import { FastifyInstance } from 'fastify';
 
 export async function getEvent(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().get(
-        '/events/:eventId',
-        {
-            schema: {
-                params: z.object({
-                    eventId: z.string().uuid(),
-                }),
-                response: {
-                    200: z.object({
-                        id: z.string().uuid(),
-                        title: z.string(),
-                        maximumAttendees: z.number().int().nullable().optional(),
-                    }),
-                    404: z.object({
-                        message: z.string(),
-                    }),
-                },
-            },
-        },
-        async (request, reply) => {
-            const { eventId } = request.params;
+	app.withTypeProvider<ZodTypeProvider>().get(
+		'/events/:eventId',
+		{
+			schema: {
+				params: z.object({
+					eventId: z.string().uuid(),
+				}),
+				response: {
+					200: z.object({
+						id: z.string().uuid(),
+						title: z.string(),
+						maximumAttendees: z.number().int().nullable().optional(),
+            attendeesRegistred: z.number().int().nullable().optional(),
+					}),
+					404: z.object({
+						message: z.string(),
+					}),
+				},
+			},
+		},
+		async (request, reply) => {
+			const { eventId } = request.params;
 
-            const event = await prisma.event.findUnique({
-                where: {
-                    id: eventId,
-                },
-            });
+			const event = await prisma.event.findUnique({
+				select: {
+					id: true,
+          title: true,
+					maximumAttendees: true,
+					_count: {
+						select: {
+							attendees: true,
+						},
+					},
+				},
+				where: {
+					id: eventId,
+				},
+			});
 
-            if (!event) {
-                reply.status(404).send({
-                    message: 'Event not found',
-                });
-                return;
-            }
+			if (!event) {
+				reply.status(404).send({
+					message: 'Event not found',
+				});
+				return;
+			}
 
-            reply.send({
-                id: event.id,
-                title: event.title,
-                maximumAttendees: event.maximumAttendees,
-            });
-        }
-    );
+			reply.send({
+				id: event.id,
+				title: event.title,
+				maximumAttendees: event.maximumAttendees,
+        attendeesRegistred: event._count.attendees,
+			});
+		}
+	);
 }
