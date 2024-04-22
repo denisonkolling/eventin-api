71b7d9aaa237362858ab7dce55612c9056a9a56b
Denison Kolling <denisonkolling@gmail.com>
2024-04-22 09:50:12 -0300

    feat: add pagination and checkin date get event attendees

--- src/routes/get-event-attendees.ts
+++ src/routes/get-event-attendees.ts
@@ -11,14 +11,21 @@ export async function getEventAttendees(app: FastifyInstance) {
 				params: z.object({
 					eventId: z.string().uuid(),
 				}),
+				querystring: z.object({
+					query: z.string().nullish(),
+					pageIndex: z.string().nullish().default('0').transform(Number),
+				}),
 				response: {
 					200: z.object({
 						id: z.string().uuid(),
 						title: z.string(),
 						attendees: z.array(
 							z.object({
+								id: z.number(),
 								name: z.string(),
 								email: z.string().email(),
+								createdAt: z.date(),
+								checkIn: z.date().nullable(),
 							})
 						),
 					}),
@@ -30,10 +37,25 @@ export async function getEventAttendees(app: FastifyInstance) {
 		},
 		async (request, reply) => {
 			const { eventId } = request.params;
+			const { pageIndex, query } = request.query;
 
 			const attendees = await prisma.attendee.findMany({
-				where: {
-					eventId,
+				select: {
+					id: true,
+					name: true,
+					email: true,
+					createdAt: true,
+					checkIn: {
+						select: {
+							createdAt: true,
+						},
+					},
+				},
+				where: query ? { eventId, name: { contains: query } } : { eventId },
+				take: 10,
+				skip: pageIndex * 10,
+				orderBy: {
+					createdAt: 'desc',
 				},
 			});
 
@@ -58,8 +80,11 @@ export async function getEventAttendees(app: FastifyInstance) {
 				id: event.id,
 				title: event.title,
 				attendees: attendees.map((attendee) => ({
+					id: attendee.id,
 					name: attendee.name,
 					email: attendee.email,
+					createdAt: attendee.createdAt,
+					checkIn: attendee.checkIn?.createdAt ?? null,
 				})),
 			});
 		}
